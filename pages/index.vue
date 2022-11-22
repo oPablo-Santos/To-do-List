2c45cde484f7ede0d329619ba5b46f4daaec7145
oPablo-Santos <pablo_p9_santos@hotmail.com>
2022-11-22 18:03:24 -0300

    To-do List
    Lista de afazeres

---
+++ pages/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <main>
+    <h1>Lista de tarefas</h1>
+    <p>Adicione itens a sua lista de tarefa</p>
+
+    <div class="create-new">
+      <input
+        type="text"
+        v-model="newTask"
+        placeholder="Adicione tarefa"
+        @keypress.enter="addTask"
+      />
+      <button @click="addTask">Add</button>
+    </div>
+    <div class="tasks">
+      <task v-for="(task, i) in $store.state.tasks" :key="i" :task="task" />
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newTask: "",
+    };
+  },
+  methods: {
+    addTask() {
+      if (this.newTask) {
+        this.$store.commit("ADD_TASK", this.newTask);
+        this.newTask = "";
+      }
+    },
+  },
+};
+</script>
